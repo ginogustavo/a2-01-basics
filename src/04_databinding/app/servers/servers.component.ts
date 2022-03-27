90f99b1b28baa8860be4d673b5c0ed4c5f3b0e01
Gino Ureta <ing.gino@gmail.com>
2022-03-27 06:36:28 -0400

    DataBinding

--- src/04_databinding/app/servers/servers.component.ts
+++ src/04_databinding/app/servers/servers.component.ts
@@ -1,8 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  selector: 'app-servers', // by element
+  // selector: '[app-servers]', // by attribute
+  // selector: '.app-servers', // by class
+
+  //Note: select by ID is not supported by Angular
+
   templateUrl: './servers.component.html',
+  // template: '<app-server></app-server><app-server></app-server>',
+  // template: `
+  //   <app-server></app-server>
+  //   <app-server></app-server>
+  // `,
   styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent implements OnInit {
