bffbe420ba331117f1fe6e4ce368c4a2be04f616
Gino Ureta <ing.gino@gmail.com>
2022-03-27 12:35:39 -0400

    nStyle

--- src/05_Directives/ngStyle/server.component.ts
+++ src/05_Directives/ngStyle/server.component.ts
@@ -3,13 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
-  styles: [
-    `
-      .online {
-        color: white;
-      }
-    `,
-  ],
 })
 export class ServerComponent {
   serverId: number = 10;
